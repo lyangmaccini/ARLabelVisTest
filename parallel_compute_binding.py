145816f18a70336a75d01553552e964ad245f24f
Autumn Tilley <118211614+autumn-tilley@users.noreply.github.com>
2025-03-18 19:42:09 -0400

    Added binding of space to an ellipsoid

--- parallel_compute_binding.py
+++ parallel_compute_binding.py
@@ -9,6 +9,8 @@ import time
 import math
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import alphashape
+from scipy.optimize import curve_fit
+import cvxpy as cp
 
 
 def RGBToLAB(RGB):
@@ -34,6 +36,101 @@ def RGBToLAB(RGB):
     b = 200 * (XYZ[:, 1] - XYZ[:, 2])
 
     return np.stack([L, a, b], axis=1)
+
+def bindLABtoEllipsoid(allLABPoints, allRGB):
+
+    # Create a 3D surface plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Find which point make up the boundary
+    shape = alphashape.alphashape(allLABPoints, alpha=0.05)
+    boundaryLABs = shape.vertices
+
+    dim  = boundaryLABs.shape[1]
+    hull = ConvexHull(boundaryLABs)
+    A = hull.equations[:,0:dim]
+    b = -hull.equations[:,dim]
+    B = cp.Variable((dim,dim), PSD=True) #Ellipsoid
+    d = cp.Variable(dim)                 #Center
+
+    constraints = [cp.norm(B@A[i],1.5)+A[i]@d<=b[i] for i in range(len(A))]
+    prob = cp.Problem(cp.Minimize(-cp.log_det(B)), constraints)
+    optval = prob.solve()
+
+    shape_matrix = np.linalg.inv(B.value.T @ B.value)
+    center = d.value
+    eigenvalues, eigenvectors = np.linalg.eig(shape_matrix)
+    rotation = eigenvectors
+    rotation_inv = np.linalg.inv(rotation)
+    axes = 1/ np.sqrt(eigenvalues)
+
+    # To graph the ellipsoid:
+    # u = np.linspace(0.0, 2.0 * np.pi, 100)
+    # v = np.linspace(0.0, np.pi, 100)
+    # x = axes[0] * np.outer(np.cos(u), np.sin(v))
+    # y = axes[1] * np.outer(np.sin(u), np.sin(v))
+    # z = axes[2] * np.outer(np.ones_like(u), np.cos(v))
+    # for i in range(len(x)):
+    #     for j in range(len(x)):
+    #         [x[i,j],y[i,j],z[i,j]] = np.dot([x[i,j],y[i,j],z[i,j]], rotation) + center
+    # ax.scatter(x, y, z, c = "blue", alpha=0.5)
+
+    # Create a copy of the original Lab Points
+    og_allLabPoints = np.copy(allLABPoints)
+
+    for i, point in enumerate(allLABPoints):
+        # If the point is outside of the binding ellipsoid, move it onto the ellipsoid using ray intersection
+        if not insideRotatedEllipsoid(point[0], point[1], point[2], shape_matrix, center):
+            point = point - center
+            point = rotation_inv @ point
+            direction = -point
+            new_point = ellipsoidIntersection(point[0], point[1], point[2], direction[0], direction[1], direction[2], 1, 1, 1)
+            new_point = rotation @ new_point
+            new_point = new_point  + center
+            allLABPoints[i] = np.asarray(new_point)
+
+    # Plot the positions of the bounded LAB points in their original RGB color
+    X = allLABPoints[:, 0]
+    Y = allLABPoints[:, 1]
+    Z = allLABPoints[:, 2]
+    # ax.scatter(X, Y, Z, c = allRGB/255.0, alpha=0.5)
+    
+    # Plot the positions of the original/unchanged LAB points in their original RGB color
+    originalX = og_allLabPoints[:, 0]
+    originalY = og_allLabPoints[:, 1]
+    originalZ = og_allLabPoints[:, 2]
+    # ax.scatter(originalX, originalY, originalZ, c = "red", alpha=0.05)
+
+    # Set labels and title
+    ax.set_xlim([-100, 100])   # Set x-axis limits
+    ax.set_ylim([-100, 100])   # Set y-axis limits
+    ax.set_zlim([-100, 100])   # Set z-axis limits
+    ax.set_box_aspect([1.0, 1.0, 1.0])
+    ax.set_xlabel("L")
+    ax.set_ylabel("a")
+    ax.set_zlabel("b")
+    ax.set_title("Ellipsoid Surface Plot")
+
+    # plt.show()
+
+    return allLABPoints
+
+def insideRotatedEllipsoid(px, py, pz, shape_matrix, center):
+    temp = np.array([px - center[0], py - center[1], pz - center[2]])
+    return temp.T @ shape_matrix @ temp - 1 < 0
+
+def ellipsoidFunc(data, a, b, c):
+    x, y, z = data
+    center = np.asarray([63, -61, 6])
+    return ((x - center[0])**2/a) + ((y - center[1])**2/b) + ((z - center[2])**2/c) - 1
+
+def ellipsoidFuncPos(center, x, y, a, b, c):
+    return c * np.sqrt(1 - (((x - center[0]) / a) ** 2 + ((y - center[1])/b) ** 2)) + center[2]
+
+def ellipsoidFuncNeg(center, x, y, a, b, c):
+    return -c * np.sqrt(1 - (((x - center[0]) / a) ** 2 + ((y - center[1])/b) ** 2)) + center[2]
+
 
 def bindLABtoSphere(allLABPoints, allRGB):
 
@@ -89,24 +186,71 @@ def bindLABtoSphere(allLABPoints, allRGB):
     x = center[0] + r * np.sin(phi) * np.cos(theta)
     y = center[1] + r * np.sin(phi) * np.sin(theta)
     z = center[2] +r * np.cos(phi)
-    #ax.plot_surface(x, y, z, color='red', alpha=0.5)
+    # ax.plot_surface(x, y, z, color='red', alpha=0.5)
 
     # Plot the positions of the bounded LAB points in their original RGB color
     X = allLABPoints[:, 0]
     Y = allLABPoints[:, 1]
     Z = allLABPoints[:, 2] 
-    ax.scatter(X, Y, Z, c = allRGB/255.0, alpha=0.15)
+    #ax.scatter(X, Y, Z, c = allRGB/255.0, alpha=0.15)
 
     #Plot the possible boundary points
     #ax.scatter(boundaryLABs[:,0], boundaryLABs[:,1], boundaryLABs[:,2], c='blue')
 
-    ax.set_xlim([-100, 50])   # Set x-axis limits
-    ax.set_ylim([-100, 50])   # Set y-axis limits
-    ax.set_zlim([-100, 50])   # Set z-axis limits
+    ax.set_xlim([-100, 100])   # Set x-axis limits
+    ax.set_ylim([-100, 100])   # Set y-axis limits
+    ax.set_zlim([-100, 100])   # Set z-axis limits
     ax.set_box_aspect([1.0, 1.0, 1.0])
+
     plt.show()
 
     return allLABPoints
+
+def insideEllipsoid(px, py, pz, a, b, c):
+    return (px ** 2) / (a ** 2) + (py ** 2) / (b ** 2) + (pz ** 2) / (c ** 2) - 1 < 0
+
+def ellipsoidIntersection(px, py, pz, dx, dy, dz, a, b, c):
+    # a, b, c = ellipsoid radii
+    # px, py, pz = original point
+    # dx, dy, dz = center - point
+
+    A1 = (b ** 2) * (c ** 2) * (dx ** 2) + (a ** 2) * (c ** 2) * (dy ** 2) + (a ** 2) * (b ** 2) * (dz ** 2)
+    B1 = 2 * (b ** 2) * (c ** 2) * px * dx + 2 * (a ** 2) * (c ** 2) * py * dy + 2 * (a ** 2) * (b ** 2) * pz * dz
+    C1 = (b ** 2) * (c ** 2) * (px ** 2) + (a ** 2) * (c ** 2) * (py ** 2) + (a ** 2) * (b ** 2) * (pz ** 2) - (a ** 2) * (b ** 2) * (c ** 2)
+    D = B1 ** 2 - 4 * A1 * C1
+    t = 0
+    if D > 0:
+        t1 = (-B1 + math.sqrt(D)) / (2 * A1)
+        t2 = (-B1 - math.sqrt(D)) / (2 * A1)
+        # print("hello")
+        # print(t1)
+        # print(t2)
+        if (t1 < 0):
+            if (t2 > 0):
+                t = t2
+            else:
+                print("warning1")
+                #if both negative no solution
+        if (t2 < 0):
+            if (t1 > 0):
+                t = t1
+            else:
+                print("warning2")
+        if (t1 > 0 and t2 > 0):
+            if (t1 < t2):
+                t = t1
+            else:
+                t = t2
+    elif D == 0:
+        t = -B1 / (2 * A1)
+        #if determinant negative no solution 
+    else:
+        print("warning3")
+
+    return [px + t * dx, py + t * dy, pz + t * dz]
+    
+    
+
 
 
 def findPointAtMaxDistance(inputRGB, allLABPoints):
@@ -130,13 +274,12 @@ def process_colors(rgb_range, allLABs):
     return CandidateLABs, CandidateRGBs
 
 
-
 def main():
     num_cpus = os.cpu_count() 
     n_processes = num_cpus - 4 # Change this to use more/less CPUs. 
     print("Number of CPUs:", num_cpus, "Number of CPUs we are using:", n_processes)
 
-    stepSize = 16 
+    stepSize = 16
     # Sample: 0, 15, 31, 45, ... 255
     allRGB = np.array([[r-1, g-1, b-1] for r in range(0, 257, stepSize)
                                for g in range(0, 257, stepSize)
@@ -144,7 +287,8 @@ def main():
     allRGB = np.where(allRGB < 0, 0, allRGB)
     allRGB = np.where(allRGB > 255, 255, allRGB)
     allLABs = RGBToLAB(allRGB)
-    boundedLABs = bindLABtoSphere(allLABs, allRGB)
+    # boundedLABs = bindLABtoEllipsoid(allLABs, allRGB)
+    boundedLABs = bindLABtoEllipsoid(allLABs, allRGB)
 
     print("number allLAB", len(allLABs))
     print("number boundedLABs", len(boundedLABs))
@@ -161,7 +305,7 @@ def main():
         CandidateLABs += lab
         CandidateRGBs += rgb
 
-    with open("CandidateLABvals.txt", "w") as f, open("CorrespondingRGBVals.txt", "w") as f2:
+    with open("CandidateLABvals_step16_76_ellipsoid.txt", "w") as f, open("CorrespondingRGBVals_step16_76_ellipsoid.txt", "w") as f2:
         for lab, rgb in zip(CandidateLABs, CandidateRGBs):
             f.write(f"{lab[0]},{lab[1]},{lab[2]}\n")
             f2.write(f"{rgb[0]},{rgb[1]},{rgb[2]}\n")
